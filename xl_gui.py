f6405b3fcba3d573908102610e1be7614febf508
Tyler Cleveland <46286834+Tyler-C2@users.noreply.github.com>
2022-06-13 13:16:55 -0400

    Update xl_gui.py
    Added auto fill feature from the image_input.py file. Made changes to how entry fields are created and placed in window.

--- xl_gui.py
+++ xl_gui.py
@@ -2,13 +2,17 @@ import tkinter as tk
 from tkinter import filedialog
 from tkcalendar import Calendar, DateEntry
 import xl_format as xl_f
+import image_input
 import os
 
 class Window(tk.Frame):
     def __init__(self, window):
         self.this_files_path = os.path.dirname(os.path.abspath(__file__))
         self.new_wb = xl_f.WorkbookCreator()
-        self.window=window
+        self.new_img_read_car_name = image_input.Top_ROI()
+        self.new_img_read_vals = image_input.Right_ROI()
+        self.selected_car = None
+        self.window = window
         self.window.title("Excel program")
         self.window.geometry("550x450")
         self.create_frames()
@@ -20,16 +24,87 @@ class Window(tk.Frame):
         self.window.config(menu=self.menu)
 
         fileMenu = tk.Menu(self.menu, tearoff=0)
-        fileMenu.add_command(label="Select File", command=self.select_path)
+        fileMenu.add_command(label="Select File", command=self.select_path_to_xl)
+        autoFillMenu = tk.Menu(self.menu, tearoff=0, )
+        autoFillMenu.add_command(label="Select Image", command=self.select_path_to_image)
         self.menu.add_cascade(label="File", menu=fileMenu)
+        self.menu.add_cascade(label="Auto Fill", menu=autoFillMenu)
 
-    def select_path(self):
-        self.window.filePath = filedialog.askopenfilename(initialdir=self.this_files_path, title="File Select", filetypes=[("Excel Files", "*.xlsx")]) 
+    def select_path_to_xl(self):
+        file_type = [("Excel Files", "*.xlsx")]
+        self.window.filePath = filedialog.askopenfilename(initialdir=self.this_files_path, title="File Select", filetypes=file_type) 
         self.new_wb.add_path(self.window.filePath)
         if self.new_wb.path != None:
             self.feedback["text"] = f"You are now working with {self.new_wb.file_name}"
         else:
             self.feedback["text"] = "No file selected."
+
+    def select_path_to_image(self):
+        file_types = [("PNG Files", "*.png"),("Jpeg Files", "*.jpeg"),("Jpg Files", "*.jpg")]
+        self.window.filePath = filedialog.askopenfilename(initialdir=self.this_files_path, title="File Select", filetypes=file_types)
+        self.new_img_read_car_name.add_path(self.window.filePath)
+        self.new_img_read_vals.add_path(self.window.filePath)
+        if self.new_img_read_vals.path != None:
+            try:
+                self.new_img_read_car_name.creator()
+                self.new_img_read_vals.creator()
+                self.car_select_popup()
+                self.window.wait_window(pop)
+                
+                if self.selected_car == 1:
+                    self.car1_name.delete(0,tk.END)
+                    self.car1_name.insert(0,self.new_img_read_car_name.vehicle_text)
+                    
+                    for i in range(len(self.car1_entry_fields)):
+                        self.car1_entry_fields[i].delete(0,tk.END)
+                        self.car1_entry_fields[i].insert(0,self.new_img_read_vals.premium_values[i])
+
+                    self.feedback["text"] = f"Car 1 has been auto filled from provided document."
+                
+                elif self.selected_car == 2:
+                    self.car2_name.delete(0,tk.END)
+                    self.car2_name.insert(0,self.new_img_read_car_name.vehicle_text)
+                    
+                    for i in range(len(self.car2_entry_fields)):
+                        self.car2_entry_fields[i].delete(0,tk.END)
+                        self.car2_entry_fields[i].insert(0,self.new_img_read_vals.premium_values[i])
+
+                    self.feedback["text"] = f"Car 2 has been auto filled from provided document."
+
+                elif self.selected_car == None :
+
+                    self.feedback["text"] = f"auto fill was not performed."
+            except:
+                self.feedback["text"] = f"file not recognized. Please check the document."
+        else:
+            self.feedback["text"] = f"auto fill was not performed."
+            
+        self.selected_car = None
+        self.new_img_read_car_name.clear()
+        self.new_img_read_vals.clear()
+
+    def car_select_popup(self):
+        global pop 
+        pop = tk.Toplevel(self.window)
+        pop.geometry("300x100")
+        pop.title("Car Select")
+        pop_label = tk.Label(pop, text="Select which car to auto fill.")
+        pop_label.pack(pady=10)
+        pop_frame = tk.Frame(pop)
+        pop_frame.pack(pady=5)
+        button1 = tk.Button(pop_frame, text= "Car 1", command=lambda: self.car_choice(1))
+        button1.grid(row=2,column=1,padx=10)
+        button2 = tk.Button(pop_frame, text= "Car 2", command=lambda: self.car_choice(2))
+        button2.grid(row=2,column=2,padx=10)
+
+    def car_choice(self,choice):
+        pop.destroy()
+        if choice == 1:
+            self.selected_car = 1
+        elif choice == 2:
+            self.selected_car = 2
+        else:
+            self.selected_car = None
 
     def create_string_vars(self):
         self.c1_items = {}
@@ -85,36 +160,23 @@ class Window(tk.Frame):
         # item labels
         self.data_label = tk.Label(self.mid_frame, text="Start/End Date : ").grid(row = 1)
         self.car_label = tk.Label(self.mid_frame, text="Car Name : ").grid(row = 3)
-        self.item1_label = tk.Label(self.mid_frame, text=f"{self.new_wb.items[0]} : ").grid(row = 4)     
-        self.item2_label = tk.Label(self.mid_frame, text=f"{self.new_wb.items[1]} : ").grid(row = 5)     
-        self.item3_label = tk.Label(self.mid_frame, text=f"{self.new_wb.items[2]} : ").grid(row = 6)
-        self.item4_label = tk.Label(self.mid_frame, text=f"{self.new_wb.items[3]} : ").grid(row = 7)
-        self.item5_label = tk.Label(self.mid_frame, text=f"{self.new_wb.items[4]} : ").grid(row = 8)
-        self.item6_label = tk.Label(self.mid_frame, text=f"{self.new_wb.items[5]} : ").grid(row = 9)
-        self.item7_label = tk.Label(self.mid_frame, text=f"{self.new_wb.items[6]} : ").grid(row = 10)  
-        self.item8_label = tk.Label(self.mid_frame, text=f"{self.new_wb.items[7]} : ").grid(row = 11)
-        
+        self.create_item_labels()
+
         # first column of entries
-        self.car1_name = tk.Entry(self.mid_frame, textvariable=self.car1_var).grid(row=3,column=2)
-        self.car1_item1 = tk.Entry(self.mid_frame, textvariable=self.c1_items['c1_item1']).grid(row=4,column=2)
-        self.car1_item2 = tk.Entry(self.mid_frame, textvariable=self.c1_items['c1_item2']).grid(row=5,column=2) 
-        self.car1_item3 = tk.Entry(self.mid_frame, textvariable=self.c1_items['c1_item3']).grid(row=6,column=2)
-        self.car1_item4 = tk.Entry(self.mid_frame, textvariable=self.c1_items['c1_item4']).grid(row=7,column=2)
-        self.car1_item5 = tk.Entry(self.mid_frame, textvariable=self.c1_items['c1_item5']).grid(row=8,column=2)
-        self.car1_item6 = tk.Entry(self.mid_frame, textvariable=self.c1_items['c1_item6']).grid(row=9,column=2) 
-        self.car1_item7 = tk.Entry(self.mid_frame, textvariable=self.c1_items['c1_item7']).grid(row=10,column=2)
-        self.car1_item8 = tk.Entry(self.mid_frame, textvariable=self.c1_items['c1_item8']).grid(row=11,column=2)
+        self.car1_entry_fields = []
+
+        self.car1_name = tk.Entry(self.mid_frame, textvariable=self.car1_var)
+        self.car1_name.grid(row=3,column=2)
+
+        self.create_entry_field(self.car1_entry_fields,self.c1_items,"c1_item",2)
         
         #second column of entries
-        self.car2_name = tk.Entry(self.mid_frame, textvariable=self.car2_var).grid(row=3,column=4)
-        self.car2_item1 = tk.Entry(self.mid_frame, textvariable=self.c2_items['c2_item1']).grid(row=4,column=4)
-        self.car2_item2 = tk.Entry(self.mid_frame, textvariable=self.c2_items['c2_item2']).grid(row=5,column=4)
-        self.car2_item3 = tk.Entry(self.mid_frame, textvariable=self.c2_items['c2_item3']).grid(row=6,column=4)
-        self.car2_item4 = tk.Entry(self.mid_frame, textvariable=self.c2_items['c2_item4']).grid(row=7,column=4)
-        self.car2_item5 = tk.Entry(self.mid_frame, textvariable=self.c2_items['c2_item5']).grid(row=8,column=4)
-        self.car2_item6 = tk.Entry(self.mid_frame, textvariable=self.c2_items['c2_item6']).grid(row=9,column=4)
-        self.car2_item7 = tk.Entry(self.mid_frame, textvariable=self.c2_items['c2_item7']).grid(row=10,column=4)
-        self.car2_item8 = tk.Entry(self.mid_frame, textvariable=self.c2_items['c2_item8']).grid(row=11,column=4)
+        self.car2_entry_fields = []
+
+        self.car2_name = tk.Entry(self.mid_frame, textvariable=self.car2_var)
+        self.car2_name.grid(row=3,column=4)
+
+        self.create_entry_field(self.car2_entry_fields,self.c2_items,"c2_item",4)
 
         #submit button
         self.button = tk.Button(self.mid_frame, text= "submit", command=self.process_entry)
@@ -123,6 +185,20 @@ class Window(tk.Frame):
         #finish button
         self.fin_button = tk.Button(self.mid_frame, text="Finish", command=self.finshed)
         self.fin_button.grid(ipadx=25,row=13, column=4)
+
+    def create_item_labels(self):
+        self.item_labels = []
+        row = 4
+        for i in range(8):
+            self.item_labels.append(tk.Label(self.mid_frame, text=f"{self.new_wb.items[i]} : ").grid(row = row))
+            row += 1
+
+    def create_entry_field(self,field_lst,text_var,item_str,column):
+        row = 4
+        for i in range(8):
+            field_lst.append(tk.Entry(self.mid_frame, textvariable=text_var[f"{item_str}{i+1}"]))
+            field_lst[i].grid(row=row,column=column)
+            row += 1
 
     def process_entry(self):
         #date processing
@@ -145,7 +221,7 @@ class Window(tk.Frame):
 
     def finshed(self):
         while self.new_wb.path == None:
-            self.select_path()
+            self.select_path_to_xl()
             
         self.new_wb.finished_input(self.new_wb.path)
 
